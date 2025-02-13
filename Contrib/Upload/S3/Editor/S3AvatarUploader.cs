5685db461817b363cb0193d7b5c85cf2bdbff67b
Neon Glowstick <neon.glowstick@proton.me>
2025-02-13 08:00:58 +0100

    Load, Save, Delete buttons for S3 config file

--- Contrib/Upload/S3/Editor/S3AvatarUploader.cs
+++ Contrib/Upload/S3/Editor/S3AvatarUploader.cs
@@ -19,43 +19,100 @@ namespace org.BasisVr.Contrib.Upload.S3
 
         private const string ConfigFile = "BasisVrS3UploadConfig.json";
         private static Button _uploadButton;
-        private static S3Config _config;
+        private static S3Config _config = new();
+
+        private static TextField _accessKeyField;
+        private static TextField _secretKeyField;
+        private static TextField _serviceUrlField;
+        private static TextField _bucketNameField;
 
         private static string ConfigDirectory => Application.persistentDataPath;
 
         private static void OnInspectorGuiCreated(BasisAvatarSDKInspector inspector)
         {
-            _config = LoadConfig();
             inspector.rootElement.Add(BuildGui(_config));
         }
 
         private static VisualElement BuildGui(S3Config config)
         {
-            var container = new VisualElement();
+            var container = new Foldout
+            {
+                text = "S3 Uploader"
+            };
 
-            var accessKeyField = PasswordField("Access key", config.AccessKey);
-            accessKeyField.RegisterValueChangedCallback(value => config.AccessKey = value.newValue);
-            container.Add(accessKeyField);
-
-            var secretKeyField = PasswordField("Secret key", config.SecretKey);
-            secretKeyField.RegisterValueChangedCallback(value => config.SecretKey = value.newValue);
-            container.Add(secretKeyField);
-
-            var serviceUrlField = Textfield("ServiceUrl", config.ServiceUrl);
-            serviceUrlField.RegisterValueChangedCallback(value => config.ServiceUrl = value.newValue);
-            container.Add(serviceUrlField);
-
-            var bucketNameField = Textfield("Bucket name", config.AvatarBucket);
-            bucketNameField.RegisterValueChangedCallback(value => config.AvatarBucket = value.newValue);
-            container.Add(bucketNameField);
+            container.Add(AddConfigFoldout(config));
 
             _uploadButton = new Button(OnClickedUpload)
             {
                 text = "Upload to bucket"
             };
-
             container.Add(_uploadButton);
+
             return container;
+        }
+
+        private static VisualElement AddConfigFoldout(S3Config config)
+        {
+            var container = new Foldout
+            {
+                text = "Config"
+            };
+
+            var crudButtons = ConfigCrudButtons();
+            container.Add(crudButtons);
+
+            _accessKeyField = PasswordField("Access key", config.AccessKey);
+            _accessKeyField.RegisterValueChangedCallback(value => config.AccessKey = value.newValue);
+            container.Add(_accessKeyField);
+
+            _secretKeyField = PasswordField("Secret key", config.SecretKey);
+            _secretKeyField.RegisterValueChangedCallback(value => config.SecretKey = value.newValue);
+            container.Add(_secretKeyField);
+
+            _serviceUrlField = Textfield("ServiceUrl", config.ServiceUrl);
+            _serviceUrlField.RegisterValueChangedCallback(value => config.ServiceUrl = value.newValue);
+            container.Add(_serviceUrlField);
+
+            _bucketNameField = Textfield("Bucket name", config.AvatarBucket);
+            _bucketNameField.RegisterValueChangedCallback(value => config.AvatarBucket = value.newValue);
+            container.Add(_bucketNameField);
+
+            return container;
+        }
+
+        private static VisualElement ConfigCrudButtons()
+        {
+            var crudButtons = new VisualElement
+            {
+                style = { flexDirection = FlexDirection.Row }
+            };
+
+            var loadButton = new Button(() =>
+            {
+                _config = LoadConfig();
+                _secretKeyField.SetValueWithoutNotify(_config.SecretKey);
+                _accessKeyField.SetValueWithoutNotify(_config.AccessKey);
+                _serviceUrlField.SetValueWithoutNotify(_config.ServiceUrl);
+                _bucketNameField.SetValueWithoutNotify(_config.AvatarBucket);
+            })
+            {
+                text = "Load",
+                style = { flexGrow = -1f }
+            };
+            crudButtons.Add(loadButton);
+            var saveButton = new Button(() => SaveConfig(_config))
+            {
+                text = "Save",
+                style = { flexGrow = -1f }
+            };
+            crudButtons.Add(saveButton);
+            var deleteButton = new Button(DeleteConfig)
+            {
+                text = "Delete",
+                style = { flexGrow = -1f }
+            };
+            crudButtons.Add(deleteButton);
+            return crudButtons;
         }
 
         private static async void OnClickedUpload()
@@ -63,8 +120,6 @@ namespace org.BasisVr.Contrib.Upload.S3
             _uploadButton.SetEnabled(false);
             try
             {
-                await SaveConfigAsync(_config, Application.exitCancellationToken);
-
                 // todo Actually upload
                 // todo Progress bar for upload of bundle and meta file
             }
@@ -77,10 +132,7 @@ namespace org.BasisVr.Contrib.Upload.S3
 
         private static S3Config LoadConfig()
         {
-            var directory = ConfigDirectory;
-            EnsureDirectoryExists(directory);
-
-            var path = Path.Combine(directory, ConfigFile);
+            var path = Path.Combine(ConfigDirectory, ConfigFile);
             if (File.Exists(path))
             {
                 var json = File.ReadAllText(path);
@@ -91,21 +143,25 @@ namespace org.BasisVr.Contrib.Upload.S3
             return new S3Config();
         }
 
-        private static Task SaveConfigAsync(S3Config config, CancellationToken cancellationToken)
+        private static void SaveConfig(S3Config config)
         {
             var directory = ConfigDirectory;
-            EnsureDirectoryExists(directory);
-
-            var path = Path.Combine(directory, ConfigFile);
-            var json = JsonUtility.ToJson(config);
-            return File.WriteAllTextAsync(path, json, cancellationToken);
-        }
-
-        private static void EnsureDirectoryExists(string directory)
-        {
             if (!Directory.Exists(directory))
             {
                 Directory.CreateDirectory(directory);
+            }
+
+            var path = Path.Combine(directory, ConfigFile);
+            var json = JsonUtility.ToJson(config);
+            File.WriteAllText(path, json);
+        }
+
+        private static void DeleteConfig()
+        {
+            var path = Path.Combine(ConfigDirectory, ConfigFile);
+            if (File.Exists(path))
+            {
+                File.Delete(path);
             }
         }
 
